2207c7f8d7e17d85f48c4707c6404c72ef7450ac
AvocadoHead <55915461+AvocadoHead@users.noreply.github.com>
2025-11-02 23:15:52 +0200

    Update UI.jsx

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -24,7 +24,7 @@ export const pageAtom = atom(0);
 
 export const pages = [
   {
-    front: "book-cover",
+    front: "כריכה שאלות לי אליך.png",
     back: pictures[0],
   },
 ];
@@ -35,65 +35,3 @@ for (let i = 1; i < pictures.length - 1; i += 2) {
     back: pictures[(i + 1) % pictures.length],
   });
 }
-
-pages.push({
-  front: pictures[pictures.length - 1],
-  back: "book-back",
-});
-
-export const UI = () => {
-  const [page, setPage] = useAtom(pageAtom);
-
-  useEffect(() => {
-    const audio = new Audio("/audios/page-flip-01a.mp3");
-    audio.play();
-  }, [page]);
-
-  return (
-    <>
-      <main className="pointer-events-none select-none z-10 fixed inset-0 flex justify-between flex-col">
-        <img
-          className="pointer-events-auto mt-8 ml-8 w-20"
-          src="/images/Optopia Eye.jpg"
-        />
-        <div className="w-full overflow-auto pointer-events-auto flex justify-center fixed bottom-0 pb-10">
-          <div className="overflow-auto flex items-center gap-4 max-w-full">
-            {[...pages].map((_, index) => (
-              <button
-                key={index}
-                className={`border-transparent hover:border-white transition-all duration-300  px-4 py-3 rounded-full  text-lg uppercase shrink-0 border ${
-                  index === page
-                    ? "bg-white/90 text-black"
-                    : "bg-black/30 text-white"
-                }`}
-                onClick={() => setPage(index)}
-              >
-                {index === 0 ? "Cover" : `${index}`}
-              </button>
-            ))}
-            <button
-              className={`border-transparent hover:border-white transition-all duration-300  px-4 py-3 rounded-full  text-lg uppercase shrink-0 border ${
-                page === pages.length
-                  ? "bg-white/90 text-black"
-                  : "bg-black/30 text-white"
-              }`}
-              onClick={() => setPage(pages.length)}
-            >
-              Back Cover
-            </button>
-          </div>
-        </div>
-        <div className="overflow-hidden bg-black pointer-events-none print:hidden fixed bottom-20 w-full">
-          <div className="flex items-center gap-8 w-full justify-center">
-            <h1 style={{transform: 'translateX(-100%)'}} className="text-white text-4xl font-black">שאלה לי אליך</h1>
-          </div>
-        </div>
-        <div className="overflow-hidden bg-black pointer-events-none print:hidden fixed bottom-32 w-full">
-          <div className="flex items-center gap-8 w-full justify-center">
-            <h1 style={{transform: 'translateX(100%)'}} className="text-white text-4xl font-black">שאלה לי אליך</h1>
-          </div>
-        </div>
-      </main>
-    </>
-  );
-};

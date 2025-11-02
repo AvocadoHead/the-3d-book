9c58273bf0300969b3a8d65b695987b7763ab836
AvocadoHead <55915461+AvocadoHead@users.noreply.github.com>
2025-11-02 15:15:25 +0200

    fix: Remove leading slash from audio and image paths

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -27,6 +27,7 @@ export const pages = [
     back: pictures[0],
   },
 ];
+
 for (let i = 1; i < pictures.length - 1; i += 2) {
   pages.push({
     front: pictures[i % pictures.length],
@@ -43,7 +44,7 @@ export const UI = () => {
   const [page, setPage] = useAtom(pageAtom);
 
   useEffect(() => {
-    const audio = new Audio("/audios/page-flip-01a.mp3");
+    const audio = new Audio("audios/page-flip-01a.mp3");
     audio.play();
   }, [page]);
 
@@ -51,35 +52,32 @@ export const UI = () => {
     <>
       <main className=" pointer-events-none select-none z-10 fixed  inset-0  flex justify-between flex-col">
         <a
-          className="pointer-events-auto mt-10 ml-10"
-          href="https://lessons.wawasensei.dev/courses/react-three-fiber"
+          className="pointer-events-auto mt-8 ml-8"
+          href="https://www.wawasensei.dev/"
         >
-          <img className="w-20" src="/images/wawasensei-white.png" />
+          <img className="w-20" src="images/wawasensei-white.png" />
         </a>
-        <div className="w-full overflow-auto pointer-events-auto flex justify-center">
-          <div className="overflow-auto flex items-center gap-4 max-w-full p-10">
-            {[...pages].map((_, index) => (
-              <button
-                key={index}
-                className={`border-transparent hover:border-white transition-all duration-300  px-4 py-3 rounded-full  text-lg uppercase shrink-0 border ${
-                  index === page
-                    ? "bg-white/90 text-black"
-                    : "bg-black/30 text-white"
-                }`}
-                onClick={() => setPage(index)}
-              >
-                {index === 0 ? "Cover" : `Page ${index}`}
-              </button>
-            ))}
+        <div className="w-full overflow-auto pointer-events-none">
+          <div className="flex items-center gap-4 justify-center md:justify-end p-8">
+            <button
+              className={`border-transparent hover:border-white transition-all duration-300  px-4 py-3 rounded-full  text-lg uppercase shrink-0 border ${
+                page === 0
+                  ? "opacity-25 pointer-events-none"
+                  : "pointer-events-auto cursor-pointer"
+              }`}
+              onClick={() => setPage(page - 1)}
+            >
+              Previous page
+            </button>
             <button
               className={`border-transparent hover:border-white transition-all duration-300  px-4 py-3 rounded-full  text-lg uppercase shrink-0 border ${
                 page === pages.length
-                  ? "bg-white/90 text-black"
-                  : "bg-black/30 text-white"
+                  ? "opacity-25 pointer-events-none"
+                  : "pointer-events-auto cursor-pointer"
               }`}
-              onClick={() => setPage(pages.length)}
+              onClick={() => setPage(page + 1)}
             >
-              Back Cover
+              Next page
             </button>
           </div>
         </div>
@@ -87,7 +85,7 @@ export const UI = () => {
 
       <div className="fixed inset-0 flex items-center -rotate-2 select-none">
         <div className="relative">
-          <div className="bg-white/0  animate-horizontal-scroll flex items-center gap-8 w-max px-8">
+          <div className="bg-white/0 animate-horizontal-scroll absolute top-0 left-0 flex items-center gap-8 w-max px-8">
             <h1 className="shrink-0 text-white text-10xl font-black ">
               Wawa Sensei
             </h1>

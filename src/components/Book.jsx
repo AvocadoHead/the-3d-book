ff3d6b09c6fded1f7316e2f86a541548ee042a03
AvocadoHead <55915461+AvocadoHead@users.noreply.github.com>
2025-11-02 23:04:26 +0200

    fix: Replace with exact working reference Book.jsx from wass08

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -36,7 +36,7 @@ const pageGeometry = new BoxGeometry(
   PAGE_HEIGHT,
   PAGE_DEPTH,
   PAGE_SEGMENTS,
-  1
+  2
 );
 
 pageGeometry.translate(PAGE_WIDTH / 2, 0, 0);
@@ -47,14 +47,15 @@ const skinIndexes = [];
 const skinWeights = [];
 
 for (let i = 0; i < position.count; i++) {
-  vertex.fromBufferAttribute(position, i);
-  const x = vertex.x;
+  // ALL VERTICES
+  vertex.fromBufferAttribute(position, i); // get the vertex
+  const x = vertex.x; // get the x position of the vertex
 
-  const skinIndex = Math.max(0, Math.floor(x / SEGMENT_WIDTH));
-  let skinWeight = (x % SEGMENT_WIDTH) / SEGMENT_WIDTH;
+  const skinIndex = Math.max(0, Math.floor(x / SEGMENT_WIDTH)); // calculate the skin index
+  let skinWeight = (x % SEGMENT_WIDTH) / SEGMENT_WIDTH; // calculate the skin weight
 
-  skinIndexes.push(skinIndex, skinIndex + 1, 0, 0);
-  skinWeights.push(1 - skinWeight, skinWeight, 0, 0);
+  skinIndexes.push(skinIndex, skinIndex + 1, 0, 0); // set the skin indexes
+  skinWeights.push(1 - skinWeight, skinWeight, 0, 0); // set the skin weights
 }
 
 pageGeometry.setAttribute(
@@ -94,10 +95,15 @@ const Page = ({ number, front, back, page, opened, bookClosed, ...props }) => {
   const [picture, picture2, pictureRoughness] = useTexture([
     `textures/${front}.jpg`,
     `textures/${back}.jpg`,
-    `textures/book-cover-roughness.jpg`,
+    ...(number === 0 || number === pages.length - 1
+      ? [`textures/book-cover-roughness.jpg`]
+      : []),
   ]);
   picture.colorSpace = picture2.colorSpace = SRGBColorSpace;
   const group = useRef();
+  const turnedAt = useRef(0);
+  const lastOpened = useRef(opened);
+
   const skinnedMeshRef = useRef();
 
   const manualSkinnedMesh = useMemo(() => {
@@ -111,7 +117,7 @@ const Page = ({ number, front, back, page, opened, bookClosed, ...props }) => {
         bone.position.x = SEGMENT_WIDTH;
       }
       if (i > 0) {
-        bones[i - 1].add(bone);
+        bones[i - 1].add(bone); // attach the new bone to the previous bone
       }
     }
     const skeleton = new Skeleton(bones);
@@ -128,6 +134,8 @@ const Page = ({ number, front, back, page, opened, bookClosed, ...props }) => {
           : {
               roughness: 0.1,
             }),
+        emissive: emissiveColor,
+        emissiveIntensity: 0,
       }),
       new MeshStandardMaterial({
         color: whiteColor,
@@ -139,9 +147,10 @@ const Page = ({ number, front, back, page, opened, bookClosed, ...props }) => {
           : {
               roughness: 0.1,
             }),
+        emissive: emissiveColor,
+        emissiveIntensity: 0,
       }),
     ];
-
     const mesh = new SkinnedMesh(pageGeometry, materials);
     mesh.castShadow = true;
     mesh.receiveShadow = true;
@@ -149,12 +158,29 @@ const Page = ({ number, front, back, page, opened, bookClosed, ...props }) => {
     mesh.add(skeleton.bones[0]);
     mesh.bind(skeleton);
     return mesh;
-  }, [picture, picture2, pictureRoughness, number]);
+  }, []);
+
+  // useHelper(skinnedMeshRef, SkeletonHelper, "red");
 
   useFrame((_, delta) => {
     if (!skinnedMeshRef.current) {
       return;
     }
+
+    const emissiveIntensity = highlighted ? 0.22 : 0;
+    skinnedMeshRef.current.material[4].emissiveIntensity =
+      skinnedMeshRef.current.material[5].emissiveIntensity = MathUtils.lerp(
+        skinnedMeshRef.current.material[4].emissiveIntensity,
+        emissiveIntensity,
+        0.1
+      );
+
+    if (lastOpened.current !== opened) {
+      turnedAt.current = +new Date();
+      lastOpened.current = opened;
+    }
+    let turningTime = Math.min(400, new Date() - turnedAt.current) / 400;
+    turningTime = Math.sin(turningTime * Math.PI);
 
     let targetRotation = opened ? -Math.PI / 2 : Math.PI / 2;
     if (!bookClosed) {
@@ -163,60 +189,70 @@ const Page = ({ number, front, back, page, opened, bookClosed, ...props }) => {
 
     const bones = skinnedMeshRef.current.skeleton.bones;
     for (let i = 0; i < bones.length; i++) {
-      const target = i === 0 ? group.current.rotation.y : targetRotation;
-      const bone = bones[i];
-      bone.rotation.y = MathUtils.lerp(
-        bone.rotation.y,
-        target,
-        delta * (i === 0 ? easingFactorFold : easingFactor)
+      const target = i === 0 ? group.current : bones[i];
+
+      const insideCurveIntensity = i < 8 ? Math.sin(i * 0.2 + 0.25) : 0;
+      const outsideCurveIntensity = i >= 8 ? Math.cos(i * 0.3 + 0.09) : 0;
+      const turningIntensity =
+        Math.sin(i * Math.PI * (1 / bones.length)) * turningTime;
+      let rotationAngle =
+        insideCurveStrength * insideCurveIntensity * targetRotation -
+        outsideCurveStrength * outsideCurveIntensity * targetRotation +
+        turningCurveStrength * turningIntensity * targetRotation;
+      let foldRotationAngle = degToRad(Math.sign(targetRotation) * 2);
+      if (bookClosed) {
+        if (i === 0) {
+          rotationAngle = targetRotation;
+          foldRotationAngle = 0;
+        } else {
+          rotationAngle = 0;
+          foldRotationAngle = 0;
+        }
+      }
+      easing.dampAngle(
+        target.rotation,
+        "y",
+        rotationAngle,
+        easingFactor,
+        delta
+      );
+
+      const foldIntensity =
+        i > 8
+          ? Math.sin(i * Math.PI * (1 / bones.length) - 0.5) * turningTime
+          : 0;
+      easing.dampAngle(
+        target.rotation,
+        "x",
+        foldRotationAngle * foldIntensity,
+        easingFactorFold,
+        delta
       );
     }
   });
 
-  useEffect(() => {
-    if (!skinnedMeshRef.current) {
-      return;
-    }
-    const bones = skinnedMeshRef.current.skeleton.bones;
-    for (let i = 0; i < bones.length; i++) {
-      const bone = bones[i];
-      if (i === 0) {
-        bone.rotation.y = opened ? -Math.PI / 2 : Math.PI / 2;
-        continue;
-      }
-      const distance = i / bones.length;
-
-      const insideCurveIntensity = opened ? distance : 1 - distance;
-
-      const outsideCurveIntensity = opened ? 1 - distance : distance;
-
-      const turningIntensity =
-        Math.sin(insideCurveIntensity * Math.PI) * turningCurveStrength;
-
-      let rotationAngle =
-        insideCurveIntensity * insideCurveStrength * Math.PI -
-        outsideCurveIntensity * outsideCurveStrength * Math.PI +
-        turningIntensity;
-
-      let foldRotationAngle = opened ? -Math.PI : Math.PI;
-      if (!bookClosed) {
-        foldRotationAngle += degToRad(number * 0.8);
-      }
-
-      if (i === 0) {
-        bone.rotation.y = foldRotationAngle;
-      } else {
-        bone.rotation.y = rotationAngle;
-      }
-
-      if (i === bones.length - 1) {
-        bone.rotation.y = -Math.PI / 2;
-      }
-    }
-  }, [bookClosed, number, opened]);
+  const [_, setPage] = useAtom(pageAtom);
+  const [highlighted, setHighlighted] = useState(false);
+  useCursor(highlighted);
 
   return (
-    <group {...props} ref={group}>
+    <group
+      {...props}
+      ref={group}
+      onPointerEnter={(e) => {
+        e.stopPropagation();
+        setHighlighted(true);
+      }}
+      onPointerLeave={(e) => {
+        e.stopPropagation();
+        setHighlighted(false);
+      }}
+      onClick={(e) => {
+        e.stopPropagation();
+        setPage(opened ? number : number + 1);
+        setHighlighted(false);
+      }}
+    >
       <primitive
         object={manualSkinnedMesh}
         ref={skinnedMeshRef}
@@ -230,9 +266,6 @@ export const Book = ({ ...props }) => {
   const [page] = useAtom(pageAtom);
   const [delayedPage, setDelayedPage] = useState(page);
 
-  const [hovered, setHovered] = useState(false);
-  useCursor(hovered);
-
   useEffect(() => {
     let timeout;
     const goToPage = () => {
@@ -240,6 +273,12 @@ export const Book = ({ ...props }) => {
         if (page === delayedPage) {
           return delayedPage;
         } else {
+          timeout = setTimeout(
+            () => {
+              goToPage();
+            },
+            Math.abs(page - delayedPage) > 2 ? 50 : 150
+          );
           if (page > delayedPage) {
             return delayedPage + 1;
           }
@@ -248,9 +287,6 @@ export const Book = ({ ...props }) => {
           }
         }
       });
-      timeout = setTimeout(() => {
-        goToPage();
-      }, 150);
     };
     goToPage();
     return () => {
@@ -259,15 +295,15 @@ export const Book = ({ ...props }) => {
   }, [page]);
 
   return (
-    <group {...props} onPointerEnter={() => setHovered(true)} onPointerLeave={() => setHovered(false)}>
+    <group {...props} rotation-y={-Math.PI / 2}>
       {[...pages].map((pageData, index) => (
         <Page
           key={index}
-          page={delayedPage}
+          {...pageData}
           number={index}
           opened={delayedPage > index}
           bookClosed={delayedPage === 0 || delayedPage === pages.length}
-          {...pageData}
+          page={delayedPage}
         />
       ))}
     </group>

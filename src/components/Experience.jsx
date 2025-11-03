8b3708df803d4e737a207085f8e0e8b1bac7c1f9
AvocadoHead <55915461+AvocadoHead@users.noreply.github.com>
2025-11-03 08:43:17 +0200

    fix: Change Book import to named import

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,5 @@
 import { Environment, Float, OrbitControls } from "@react-three/drei";
-import Book from "./Book";
+import { Book } from "./Book";
 
 export const Experience = () => {
   return (
@@ -14,7 +14,7 @@ export const Experience = () => {
       </Float>
       <OrbitControls />
       <Environment preset="studio" />
-      <directionalLight
+      <DirectionalLight
         position={[2, 5, 2]}
         intensity={2.5}
         castShadow
@@ -22,7 +22,7 @@ export const Experience = () => {
         shadow-mapSize-height={2048}
         shadow-bias={-0.0001}
       />
-      <mesh position-y={-1.5} rotation-x={-Math.PI / 2} receiveShadow>
+      <mesh position-y={-1.5} receiveShadow rotation-x={-Math.PI / 2}>
         <planeGeometry args={[100, 100]} />
         <shadowMaterial transparent opacity={0.2} />
       </mesh>

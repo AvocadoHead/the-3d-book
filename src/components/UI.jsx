c65c3bd319745d74fe47a02c262b057a34052625
AvocadoHead <55915461+AvocadoHead@users.noreply.github.com>
2025-11-03 11:01:33 +0200

    Update UI.jsx

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -126,25 +126,25 @@ export const UI = () => {
           </div>
           <div className="absolute top-0 left-0 bg-white/0 animate-horizontal-scroll-2 flex items-center gap-8 px-8 w-max">
             <h2 className="shrink-0 text-white text-10xl font-black ">
-              Wawa Sensei
+              Essential
             </h2>
             <h2 className="shrink-0 text-white text-8xl italic font-light">
-              React Three Fiber
+              מהות
             </h2>
             <h2 className="shrink-0 text-white text-12xl font-bold">
-              Three.js
+              Value
             </h2>
             <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-              Ultimate Guide
+              ערך
             </h2>
             <h2 className="shrink-0 text-white text-9xl font-medium">
-              Tutorials
+              Thank You
             </h2>
             <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-              Learn
+              תודה
             </h2>
             <h2 className="shrink-0 text-13xl font-bold">
-              Practice
+              Explain
             </h2>
             <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
               Creative

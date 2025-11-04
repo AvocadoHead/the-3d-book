93cdd851f5f807c9175d28be2fd646458febddb4
AvocadoHead <55915461+AvocadoHead@users.noreply.github.com>
2025-11-04 17:18:59 +0200

    Revert Experience.jsx to working version with OrbitControls
    Reverting from PresentationControls back to OrbitControls with proper constraints (maxPolarAngle, minPolarAngle, enablePan) to fix the broken 3D book interaction. This restores the working state from commit 1aa32a0 (Nov 3, 2025).

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,33 +1,19 @@
-import { degToRad } from 'three/src/math/MathUtils.js';
-import { useAtom } from 'jotai';
-import { editModeAtom } from './UI';
-import { Environment, Float, PresentationControls } from "@react-three/drei";
-import { EffectComposer, DepthOfField } from '@react-three/postprocessing';
+import { OrbitControls, Environment } from "@react-three/drei";
 import { Book } from "./Book";
-import { FloatingEditorPage } from './FloatingEditorPage';
 
 export const Experience = () => {
-  const [isEditing, setIsEditing] = useAtom(editModeAtom);
-
   return (
     <>
-      <PresentationControls
-        snap={true}
-        global
-        config={{mass: 1, tension: 280, friction: 30}}
-        rotation={[degToRad(-22.5), 0, 0]}
-        polar={[-0.3, 0.3]}
-        azimuth={[-0.6, 0.6]}
-      >
-        <Float
-          position={[0.64, 0, 0]}
-          floatIntensity={1}
-          speed={2}
-          rotationIntensity={2}
-        >
-          <Book />
-        </Float>
-      </PresentationControls>
+      <OrbitControls
+        target={[0.64, 0, 0]}
+        enableDamping
+        dampingFactor={0.05}
+        minDistance={2}
+        maxDistance={10}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={0}
+        enablePan={false}
+      />
       <Environment preset="studio" />
       <directionalLight
         position={[2, 5, 2]}
@@ -41,17 +27,7 @@ export const Experience = () => {
         <planeGeometry args={[100, 100]} />
         <shadowMaterial transparent opacity={0.2} />
       </mesh>
-      {isEditing && (
-        <FloatingEditorPage
-          onClose={() => setIsEditing(false)}
-          onSave={() => setIsEditing(false)}
-        />
-      )}
-      {isEditing && (
-        <EffectComposer>
-          <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} />
-        </EffectComposer>
-      )}
+      <Book />
     </>
   );
 };

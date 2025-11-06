81b468cf2905fb6a991840b94d85d8ee17e14c1d
AvocadoHead <55915461+AvocadoHead@users.noreply.github.com>
2025-11-06 10:51:01 +0200

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,35 +1,17 @@
 import { Environment, Float, OrbitControls } from "@react-three/drei";
 import { Book } from "./Book";
-import { useThree } from "@react-three/fiber";
-import { useEffect } from "react";
-
 export const Experience = () => {
-  const { camera } = useThree();
-
-  useEffect(() => {
-    camera.up.set(0, 0, 1);
-    camera.updateProjectionMatrix();
-  }, [camera]);
-
   return (
     <>
       <Float
-        rotation-x={-Math.PI / 8}
-        floatIntensity={0.5}
-        speed={1.5}
-        rotationIntensity={0.5}
+        rotation-x={-Math.PI / 4}
+        floatIntensity={1}
+        speed={2}
+        rotationIntensity={2}
       >
         <Book />
       </Float>
-      <OrbitControls 
-        enableDamping={true}
-        dampingFactor={0.05}
-        minPolarAngle={Math.PI / 3}
-        maxPolarAngle={Math.PI / 1.8}
-        minAzimuthAngle={-Math.PI / 4}
-        maxAzimuthAngle={Math.PI / 4}
-        target={[0, 0, 0]}
-      />
+      <OrbitControls />
       <Environment preset="studio"></Environment>
       <directionalLight
         position={[2, 5, 2]}
